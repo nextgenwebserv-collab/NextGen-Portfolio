2b39f49fe74b329ffd15101c722cab7c7b1e82e2
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2026-01-31 21:01:15 +0530

    First

---
+++ src/pages/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1 className="text-xl text-blue-600">Home Page</h1>
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-xl text-blue-600">Home Page</h1>
+}
